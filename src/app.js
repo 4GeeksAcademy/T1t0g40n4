724ebd4afdef8acc51fc47f36faa9a0bda7a8b3b
T1t0-G40n4 <titogaona84@gmail.com>
2025-02-17 14:12:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function() { 
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed on", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "wen I was praying"];
+    
+    let whoExcuse = Math.floor(Math.random() * who.length);
+    let actionExcuse = Math.floor(Math.random()* action.length);
+    let whatExcuse = Math.floor(Math.random() * what.length);
+    let whenExcuse = Math.floor(Math.random()*when.length);
+    
+    let excuse = `${who[whoExcuse]} ${action[actionExcuse]} ${what[whatExcuse]} ${when[whenExcuse]}.`;
+  
+    document.getElementById("excuse").innerHTML = excuse;
+    console.log(whoIndex,actionIndex,whatIndex,whenIndex);
+}
+generateExcuse();
+}
